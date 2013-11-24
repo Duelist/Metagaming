3a4a906a8d36cad116a683c88fb8a06de65b5631
Ian Benedict <ianbenedict@gmail.com>
2013-11-24 07:19:05 -0500

    New structure using Foundation

---
+++ resources/js/client.js
@@ -0,0 +1,45 @@
+/*global $, document*/
+
+(function () {
+  "use strict"
+
+  $(document).foundation();
+
+  $(document).ready(function () {
+    var socket = io.connect('http://localhost');
+
+    socket.on('receive-chat', function (data) {
+      console.log(data);
+      var message_obj = data['message'];
+      $('.chat-box').val([$('.chat-box').val(),'\n',[message_obj['user'],':',message_obj['message']].join(' ')].join(''));
+    });
+
+    socket.on('connected-users', function (data) {
+      $('.user-connect-wrapper').addClass('hide');
+      $('.chat-input-wrapper').removeClass('hide');
+      $('.user-list').val(_.reduce(data['users'], function (x, y) { return [x, y].join('\n'); }));
+    });
+
+
+    $('.user-connect').on("click", function() {
+      socket.emit('user-connect', { user: $('.user-name').val() });
+    });
+
+    $('.chat-input').on("focus", function () {
+      // Set focus state
+    });
+
+    $('.chat-input').on("change", function () {
+      // Set typing state
+    });
+
+    $('.chat-input-send').on("click", function () {
+      socket.emit('send-chat', { user: $('.user-name').val(), message: $('.chat-input').val()});
+      $('.chat-input').val('');
+    });
+
+    $(window).unload(function () {
+      socket.emit('user-disconnect', { user: $('.user-name').val() });
+    });
+  });
+})();

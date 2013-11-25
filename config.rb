13edf8b3fdac790bbcf1263583bd0b6fb9ba822a
Ian Benedict <ianbenedict@gmail.com>
2013-11-25 07:46:48 -0500

    Reorganized structure.

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@ add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "javascripts"
+css_dir = "resources/css"
+sass_dir = "resources/scss"
+images_dir = "resources/img"
+javascripts_dir = "resources/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
